2438d38826dedbdf6e93877b3de215180ce193c1
edf42001 <ethandf2@gmail.com>
2020-02-05 10:07:13 -0500

    Add new logos to gui

--- BuildEXE.bat
+++ BuildEXE.bat
@@ -1,4 +1,4 @@
-pyinstaller --onefile --name SwitchControllerGUI --icon=GUIs/Images/favicon.ico --windowed main.py
+pyinstaller --onefile --name SwitchControllerGUI --icon=GUIs/Images/Bear.ico --windowed main.py
 
 
 :: This version bundles the resources into the .exe
